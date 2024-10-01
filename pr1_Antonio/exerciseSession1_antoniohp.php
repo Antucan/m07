04cfc6197d8329ffd1462daacb3a592457f15176
Antucan <antherpra@gmail.com>
2024-10-01 16:49:31 +0200

    creating file

--- pr1_Antonio/exerciseSession1_antoniohp.php
+++ pr1_Antonio/exerciseSession1_antoniohp.php
@@ -1,1 +1,14 @@
+<!DOCTYPE html>
+<html>
 
+<body>
+    <h1>Práctica Pr1 – Intro PHP - Antonio Herrero</h2>
+        <h2>Exercise Session 1</h2>
+        <?php
+        echo "<b>1. Declare dos variables numéricas, las sume, reste, dividí y muestre los valores de las
+variables y los resultados de sus operaciones</b><br>";
+        ?>
+
+</body>
+
+</html>
