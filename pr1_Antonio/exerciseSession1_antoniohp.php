8fce0ad3daa041860ad1f15bb95db62fd2dda85d
Antucan <antherpra@gmail.com>
2024-10-01 18:48:49 +0200

    saving exerciseSession1_antoniohp

--- pr1_Antonio/exerciseSession1_antoniohp.php
+++ pr1_Antonio/exerciseSession1_antoniohp.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html>
+<?php
+session_start();
+?>
 
 <body>
         <h1>Práctica Pr1 – Intro PHP - Antonio Herrero</h2>
@@ -12,26 +15,46 @@
                 echo "<b>c) Se debe controlar que no se pueden quitar mas unidades de las que haya, en ese
 caso mostrar error.</b><br><br>";
                 ?>
-                <h2>Supermarket management</h2>
-                Worker name:<input type="text" id="name" required>
-                <h2>Choose product:</h2>
-                <select name="products" id="products" required>
-                        <option value="soft">Soft Drink</option>
-                        <option value="milk">Milk</option>
-                </select>
-                <h2>Product quantity:</h2>
-                <input type="number" required><br><br>
-                <button id="add">add</button>
-                <button id="remove">remove</button>
-                <button id="reset">reset</button>
-                <h2>Inventary:</h2>
+                <form action="" method="post">
+                        <h2>Supermarket management</h2>
+                        Worker name:<input type="text" id="name" required>
+                        <h2>Choose product:</h2>
+                        <select name="products" id="products" required>
+                                <option value="soft">Soft Drink</option>
+                                <option value="milk">Milk</option>
+                        </select>
+                        <h2>Product quantity:</h2>
+                        <input type="number" id="number" required><br><br>
+                        <input type="submit" id="add" name="add" value="add">
+                        <input type="submit" id="remove" name="remove" value="remove">
+                        <input type="reset" value="reset">
+                </form>
+                <h2>Inventory:</h2>
                 <div id="result"></div>
                 <?php
-                if (isset($_POST[""])) {
-                
-                }else{
-                        echo "<div id='result'>EMPTY FIELDS</div>";
+                $name = "";
+                $milk = 0;
+                $softDrink = 0;
+
+                if (isset($_POST["add"])) {
+                        if (isset($_POST["name"])) {
+                                $name = $_POST["name"];
+                        }
+                        if (isset($_POST["products"])) {
+                                $product = $_SESSION["products"];
+                        }
+                        if (isset($_POST["number"])) {
+                                $quantity = $_SESSION["number"];
+                        }
+                        if ($product == "milk") {
+                                $_SESSION["milk"] += $milk;
+                        } elseif ($product == "soft") {
+                                $_SESSION["soft"] += $softDrink;
+                        }
                 }
+                echo "Worker: $name<br><br>";
+                echo "Units Milk: $milk<br><br>";
+                echo "Units Soft Drink: $softDrink";
                 ?>
 </body>
 
